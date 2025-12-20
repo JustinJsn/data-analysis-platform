a7c18e34d3fab0a881d9ae09569db78672f8ee0f
JustinJsn <1278164528@qq.com>
2025-12-20 12:39:13 +0800

    feat: tailwindcss config

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import App from './App.vue';
 import './style.css';
-import './assets/styles/global.scss';
 
 const pinia = createPinia();
 const app = createApp(App);
