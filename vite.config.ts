bb30294b14e483eaaf016eac821e57094d9c9326
JustinJsn <1278164528@qq.com>
2025-12-20 11:05:53 +0800

    feat: oxclint

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import vue from '@vitejs/plugin-vue'
-import AutoImport from 'unplugin-auto-import/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
-import Components from 'unplugin-vue-components/vite'
-import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue';
+import AutoImport from 'unplugin-auto-import/vite';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
+import Components from 'unplugin-vue-components/vite';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -15,4 +15,4 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()],
     }),
   ],
-})
+});
